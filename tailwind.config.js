9e8aaeb43524501a04749558e53cacc3d9117fcd
abrormamurov <mabrorbk@gmail.com>
2024-02-07 14:23:55 +0500

    Recipe

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      sans: ["Outfit", "sans-serif"],
+      serif: ["young-serif", "serif"],
+    },
+    extend: {
+      colors: {
+        WengeBrown: "#5F564D",
+        dark: "#7A284E",
+        BrandyRed: "#854632",
+      },
+      backgroundColor: {
+        main_pink: "#f3e5d7",
+        snow: "#FFF7FB",
+      },
+    },
+  },
+  plugins: [],
+};
